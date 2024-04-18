1511b707a68ce5def5940451bee969c27c635ff5
Matthew Quijano <mq022002@gmail.com>
2024-04-18 05:26:14 -0400

    refactor: Update analysis version to match new code, without docs

--- To_Turn_In/main_analysis_version.c
+++ To_Turn_In/main_analysis_version.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+
+int fileExists(const char *filename)
+{
+    struct _stat buffer;
+    return (_stat(filename, &buffer) == 0);
+}
 
 char *replaceWordInString(const char *str, const char *searchWord, const char *replaceWord)
 {
@@ -10,35 +17,55 @@ char *replaceWordInString(const char *str, const char *searchWord, const char *r
 
     int count = 0;
     const char *tmp = str;
-    const char *original_str = str;
     while ((tmp = strstr(tmp, searchWord)) != NULL)
     {
         count++;
         tmp += searchWordLen;
     }
 
-    char *newStr = malloc(strlen(str) + count * diff + 1);
+    int multiplication = count * diff;
+    if ((diff > 0 && (multiplication < 0 || multiplication / diff != count)) ||
+        (diff < 0 && multiplication > 0))
+    {
+        fprintf(stderr, "Error: Integer overflow\n");
+        return NULL;
+    }
+
+    int newLength = strlen(str) + multiplication + 1;
+    if (newLength < 0)
+    {
+        fprintf(stderr, "Error: Integer overflow\n");
+        return NULL;
+    }
+
+    char *newStr = malloc(newLength);
     if (newStr == NULL)
     {
-        perror("Error allocating memory");
+        printf("Error allocating memory");
         return NULL;
     }
 
     char *current = newStr;
+    int remainingSpace = newLength;
     while (*str)
     {
-        if (strstr(str, searchWord) == str &&
-        (*(str - 1) == ' ' || str == original_str) && 
-        (*(str + searchWordLen) == ' ' || *(str + searchWordLen) == '\0' ||
-         *(str + searchWordLen) == ',' || *(str + searchWordLen) == '.')) 
+        if (strstr(str, searchWord) == str)
         {
-            strcpy(current, replaceWord);
+            if (replaceWordLen > remainingSpace)
+            {
+                printf("Not enough space in the buffer");
+                free(newStr);
+                return NULL;
+            }
+            strncpy(current, replaceWord, replaceWordLen);
             current += replaceWordLen;
+            remainingSpace -= replaceWordLen;
             str += searchWordLen;
         }
         else
         {
             *current++ = *str++;
+            remainingSpace--;
         }
     }
 
@@ -53,33 +80,57 @@ void processFile(FILE *input, FILE *output, const char *searchWord, const char *
     char *buffer = malloc(bufferSize * sizeof(char));
     if (buffer == NULL)
     {
-        perror("Error: memory allocation failed\n");
+        printf("Error: memory allocation failed\n");
         exit(1);
     }
     while (fgets(buffer, bufferSize, input) != NULL)
     {
+        if (strlen(buffer) >= bufferSize)
+        {
+            printf("Error: Buffer overflow\n");
+            free(buffer);
+            return;
+        }
         char *newBuffer = replaceWordInString(buffer, searchWord, replaceWord);
         if (newBuffer != NULL)
         {
             fputs(newBuffer, output);
             free(newBuffer);
         }
+        else
+        {
+            free(buffer);
+            return;
+        }
     }
     free(buffer);
 }
 
-
 int main(int argc, char *argv[])
 {
-    if (argc != 4)
+    if (argc != 5)
     {
-        printf("command takes 3 arguments. [file to search] [word to search for] [word to replace with]\n");
+        printf("command takes 4 arguments. [file to search] [word to search for] [word to replace with] [output file]\n");
         return 1;
     }
 
     char *file = argv[1];
     char *searchWord = argv[2];
     char *replaceWord = argv[3];
+    char *outputFile = argv[4];
+
+    if (fileExists(outputFile))
+    {
+        char response[4];
+        printf("The file %s already exists. Do you want to overwrite it? (yes/no): ", outputFile);
+        fgets(response, sizeof(response), stdin);
+        response[strcspn(response, "\n")] = 0;
+        if (strcmp(response, "yes") != 0)
+        {
+            printf("Aborted by user.\n");
+            return 1;
+        }
+    }
 
     if (strlen(searchWord) == 0 || strlen(replaceWord) == 0)
     {
@@ -94,16 +145,18 @@ int main(int argc, char *argv[])
     }
 
     printf("Searching for '%s' and replacing with '%s' in file '%s'\n", searchWord, replaceWord, file);
+
     FILE *fp = fopen(file, "r");
     if (fp == NULL)
     {
-        perror("Error opening file");
+        printf("Error opening file");
         return 1;
     }
-    FILE *temp = fopen("modifiedText.txt", "w");
+    FILE *temp = fopen(outputFile, "w");
     if (temp == NULL)
     {
-        perror("Error: replace file could not be created\n");
+        printf("Error: replace file could not be created\n");
+        fclose(fp);
         return 1;
     }
     processFile(fp, temp, searchWord, replaceWord);
